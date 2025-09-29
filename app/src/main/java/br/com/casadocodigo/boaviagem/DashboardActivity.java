6c140ad1f7a7173638477d96fc1e4f89289c3cc6
Diogo Ponte <diogo_ponte20@hotmail.com>
2025-09-29 20:22:07 -0300

    Criacao das telas de Login e Menu Principal

---
+++ app/src/main/java/br/com/casadocodigo/boaviagem/DashboardActivity.java
@@ -0,0 +1,24 @@
+package br.com.casadocodigo.boaviagem;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class DashboardActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+
+    public void selecionarOpcao(View view) {
+        TextView textView = (TextView) view;
+        String opcao = "Opção: "+ textView.getText().toString();
+        Toast.makeText(this, opcao, Toast.LENGTH_SHORT).show();
+    }
+}
