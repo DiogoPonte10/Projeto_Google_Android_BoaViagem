f49b4210d3f344a33c08d878c8ffd86d76925380
Diogo Ponte <diogo_ponte20@hotmail.com>
2025-11-04 23:08:11 -0300

    Complemento das constantes

--- app/src/main/java/br/com/casadocodigo/boaviagem/Constantes.java
+++ app/src/main/java/br/com/casadocodigo/boaviagem/Constantes.java
@@ -3,5 +3,8 @@ package br.com.casadocodigo.boaviagem;
 public class Constantes {
     public static final int VIAGEM_LAZER = 1;
     public static final int VIAGEM_NEGOCIOS = 2;
-    public static String VIAGEM_ID;
+    public static final String VIAGEM_ID = "viagem_id";
+    public static final String MODO_VIAGEM = "modo_viagem";
+    public static final String VIAGEM_DESTINO = "viagem_destino";
+    public static final String MODO_SELECIONAR_VIAGEM = "modo_selecionar_viagem";
 }
